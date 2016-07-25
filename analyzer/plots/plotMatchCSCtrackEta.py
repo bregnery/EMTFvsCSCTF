34038e6816a67deda08aa83ca158383169ddd386
bregnery <bregnery@ufl.edu>
2016-07-25 17:19:56 -0400

    Created more Useful Histograms

---
+++ analyzer/plots/plotMatchCSCtrackEta.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import glob
+import ROOT as root
+from src.helpers import *
+
+root.gROOT.SetBatch(True)
+
+# Open root files
+data = root.TFile("../Data_Comparison_Hist.root")
+
+# Create TCanvas
+canvas = root.TCanvas()
+
+# Access Histograms
+Hist = data.Get("matchCSCTFtrackEtaHist")
+
+Hist.SetFillColor(root.kOrange)
+Hist.SetLineColor(root.kOrange)
+Hist.SetMarkerStyle(0)
+Hist.SetFillStyle(1)
+
+# Create a 2D Histogram for more control
+xMin = 0
+xMax = 6
+yMin = 0
+yMax = 10000
+xTitle = "#eta"
+
+# Log y axis
+#canvas.SetLogy(True)
+
+axisHist = root.TH2F("axisHist","Matched CSCTF Tracks #eta",1,xMin,xMax,1,yMin,yMax)
+axisHist.GetXaxis().SetTitle(xTitle)
+axisHist.GetYaxis().SetTitle("Events/Bin")
+axisHist.GetYaxis().SetTitleOffset(1.7)
+axisHist.Draw()
+
+root.gStyle.SetOptStat(1111) # Needed for stats box
+
+Hist.Draw() # Must first just draw to get stats box
+
+canvas.GetPad(0).Update()
+stats_data = Hist.GetListOfFunctions().FindObject("stats").Clone("stats_data")
+
+axisHist.Draw()  # to get axis titles back
+stats_data.Draw()
+
+Hist.Draw("hist same")
+canvas.SaveAs("Hist_matchCSCTFtrackEta.png")
